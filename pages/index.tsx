9eb04fdec9314303491c1945694ee5255c699e3e
HanSeong Lee <jkhanseong@gmail.com>
2022-03-10 20:47:34 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head';
+import Container from "../components/Container";
+import AdviceContainer from "../containers/AdviceContainer";
 
 const Home: NextPage = () => {
   return (
@@ -54,6 +56,9 @@ const Home: NextPage = () => {
         </Head>
 
         <main>
+          <Container>
+            <AdviceContainer />
+          </Container>
         </main>
       </>
   );
