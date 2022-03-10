888f5a7f4a0113a4e01ce01ac2481a89202fd91e
HanSeong Lee <jkhanseong@gmail.com>
2022-03-10 20:46:40 +0900

    ADD | Card on desktop

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,34 +1,50 @@
 import React, {MouseEventHandler} from "react";
 import styles from './style.module.scss';
+import cn from 'classnames';
 
 type IProps = {
     subtitle: string;
     title: string;
+    loading: boolean;
     onClick?: MouseEventHandler | undefined;
 };
 
-const Card: React.FC <IProps> = ({ subtitle, title, onClick }) => {
+const Card: React.FC <IProps> = ({ subtitle, title, loading, onClick }) => {
     return (
-        <div className={styles.card}>
-            <div className={styles.subtitle}>
-                {subtitle}
+        <div className={cn(styles.card, {
+            [styles.loading]: loading,
+        })}>
+            <div className={styles.body}>
+                {!loading && (
+                    <>
+                        <div className={styles.subtitle}>
+                            {subtitle}
+                        </div>
+                        <h2 className={styles.title}>
+                            {title}
+                        </h2>
+                    </>
+                )}
+                <picture>
+                    <source srcSet={'/img/pattern-divider-desktop.svg'}
+                            media={'(min-width: 992px)'}
+                    />
+
+                    <img src={'/img/pattern-divider-mobile.svg'}
+                         alt={''}
+                    />
+                </picture>
             </div>
-            <h2 className={styles.title}>
-                {title}
-            </h2>
-            <picture>
-                <img src={'/img/pattern-divider-mobile.svg'}
-                     alt={''}
-                />
-            </picture>
-            <button className={styles.button}
-                    type={'button'}
-                    onClick={onClick}
-            >
-                <img src={'/img/icon-dice.svg'}
-                     alt={'dice'}
-                />
-            </button>
+            <div className={styles.footer}>
+                <button className={styles.button}
+                        type={'button'}
+                        onClick={onClick}
+                >
+                    <img src={'/img/icon-dice.svg'}
+                         alt={'dice'}
+                    />
+                </button>
+            </div>
         </div>
     );
 };
